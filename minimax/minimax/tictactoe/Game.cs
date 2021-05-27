04284bc2a1e5459c0c0a83d37d36353542253326
JonnyNasufi <84791948+JonnyNasufi@users.noreply.github.com>
2021-05-27 11:37:22 +0200

    Creazione classe Game

---
+++ minimax/minimax/tictactoe/Game.cs
@@ -0,0 +1,45 @@
+﻿using minimax.core.adversarial;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace minimax.tictactoe
+{
+    class Game : IGame<State, Action, Player>
+    {
+        public List<Action> GetActions(State state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public State GetInitialState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player GetPlayer(State state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player[] GetPlayers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public State GetResult(State state, Action action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetUtility(State state, Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsTerminal(State state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
