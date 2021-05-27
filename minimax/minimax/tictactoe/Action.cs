67c83a1cdbc9adf2a45da6c5fd182c477d8c79ee
Nicola Graziotin <84791589+Nicola-Graziotin@users.noreply.github.com>
2021-05-27 11:30:51 +0200

    Creazione classe Action

---
+++ minimax/minimax/tictactoe/Action.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace minimax.tictactoe
+{
+    class Action
+    {
+        public int row;
+        public int col;
+
+        public Action(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+        public int Row
+        {
+            get { return row; }
+        }
+
+        public int Col
+        {
+            get { return col; }
+        }
+    }
+}
