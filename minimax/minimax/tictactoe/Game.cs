043e5f58a92e8338478cd3adda7142cbc029e160
FraMarca <84791548+FraMarca@users.noreply.github.com>
2021-05-27 11:42:11 +0200

    Add files via upload

--- minimax/minimax/tictactoe/Game.cs
+++ minimax/minimax/tictactoe/Game.cs
@@ -29,7 +29,22 @@ namespace minimax.tictactoe
 
         public State GetResult(State state, Action action)
         {
-            throw new NotImplementedException();
+            //NUOVO STATO COPIA
+            State copyState = new State();
+            copyState.campo = (int[,])state.campo.Clone();
+            copyState.giocatoreCorrente = state.giocatoreCorrente;
+
+            //ASSEGNARE LA MOSSA
+            copyState.campo[action.Row, action.Col] = (int)copyState.giocatoreCorrente;
+
+            //INVERSIONE DEI PLAYER CORRENTI
+            if (copyState.giocatoreCorrente == Player.Circle)
+
+                copyState.giocatoreCorrente = Player.Cross;
+            else
+                copyState.giocatoreCorrente = Player.Circle;
+
+            return copyState;
         }
 
         public double GetUtility(State state, Player player)
@@ -39,7 +54,60 @@ namespace minimax.tictactoe
 
         public bool IsTerminal(State state)
         {
-            throw new NotImplementedException();
-        }
+            if (ControllaVincitore(state) != -1)
+            {
+                return true;
+            }
+            //CONTROLLO BOARD PIENA
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (state.campo[i, j] == -1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+
+        } 
+
+        public int ControllaVincitore(State state)
+        {
+            //CONTROLLO RIGHE
+            for (int i = 0; i < 3; i++)
+            {
+                if (state.campo[i, 0] != -1)
+                {
+                    if ((state.campo[i, 0] == state.campo[i, 1]) && (state.campo[i, 0] == state.campo[i, 2]))
+                    {
+                        return state.GetBoardState(i, 0);
+                    }
+                }
+                if (state.campo[0, i] != -1)
+                {
+                    if ((state.campo[0, i] == state.campo[1, i]) && (state.campo[0, i] == state.campo[2, i]))
+                    {
+                        return state.GetBoardState(0, i);
+                    }
+                }
+            }
+
+            //CONTROLLO DIAGONALI
+            if (state.campo[1, 1] != -1)
+            {
+                if ((state.campo[0, 0] == state.campo[1, 1]) && (state.campo[1, 1] == state.campo[2, 2]))
+                {
+                    return state.GetBoardState(1, 1);
+                }
+                else if ((state.campo[0, 2] == state.campo[1, 1]) && (state.campo[1, 1] == state.campo[2, 0]))
+                {
+                    return state.GetBoardState(1, 1);
+                }
+            }
+            return -1;
+        } 
     }
 }
