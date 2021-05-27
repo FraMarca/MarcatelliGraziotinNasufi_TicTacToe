f7033b193a897b738c4f2d456bd10e97d4d4c603
FraMarca <84791548+FraMarca@users.noreply.github.com>
2021-05-27 11:27:49 +0200

    Creazione classe State

---
+++ minimax/minimax/tictactoe/State.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace minimax.tictactoe
+{
+    class State
+    {
+        public const int EMPTY = -1;
+        public const int CYRCLE = (int)Player.Circle;
+        public const int CROSS = (int)Player.Cross;
+
+        public int[,] campo { get; set; }
+        public Player giocatoreCorrente { get; set; }
+
+        public State()
+        {
+            campo = new int[3, 3];
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    campo[row, col] = EMPTY;
+                }
+            }
+
+            giocatoreCorrente = Player.Cross;
+        }
+
+        public int GetBoardState(int row, int col)
+        {
+            if (campo[row, col] == EMPTY)
+            {
+                return EMPTY;
+            }
+            else if (campo[row, col] == CYRCLE)
+            {
+                return CYRCLE;
+            }
+            else
+            {
+                return CROSS;
+            }
+        }
+    }
+}
