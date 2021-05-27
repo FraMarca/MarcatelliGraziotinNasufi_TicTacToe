cf9dfa7800806c81df65c0d6b561aeb87823b1c5
JonnyNasufi <84791948+JonnyNasufi@users.noreply.github.com>
2021-05-27 12:03:18 +0200

    Definizione classe Program
    Creazione del gioco e dello stato iniziale.
    Creazione ciclo di vita del gioco.

---
+++ minimax/minimax/tictactoe/Program.cs
@@ -0,0 +1,31 @@
+﻿using minimax.core.adversarial;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace minimax.tictactoe
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new Game();
+            State state = game.GetInitialState();
+            List<Action> availableActions = new List<Action>();
+
+            AdversarialSearch<State, Action> adversarialSearch;
+
+
+            Console.WriteLine("Inserisci il livello di difficoltà dell'avversario:");
+            int.TryParse(Console.ReadLine(), out int livello);
+
+            do
+            {
+                Console.WriteLine("Inserisci la mossa che vuoi fare: ([0 1] -> RIGA: 0 COLONNA: 1) ");
+                string mossa = Console.ReadLine();
+                string[] mosse = mossa.Split(' ');
+                Action move = new Action(Convert.ToInt32(mosse[0]), Convert.ToInt32(mosse[1]));
+            } while (!game.IsTerminal(state));
+        }
+    }
+}
