639cb8d43f65dd4b84fe5e63009d86f97f0cc30f
Nicola Graziotin <84791589+Nicola-Graziotin@users.noreply.github.com>
2021-05-27 12:06:03 +0200

    Creazione metodo PrintBoardState
    Metodo che aggiorna e stampa lo stato del campo di gioco.

--- minimax/minimax/tictactoe/Program.cs
+++ minimax/minimax/tictactoe/Program.cs
@@ -27,5 +27,38 @@ namespace minimax.tictactoe
                 Action move = new Action(Convert.ToInt32(mosse[0]), Convert.ToInt32(mosse[1]));
             } while (!game.IsTerminal(state));
         }
+        public static void PrintBoardState(State state)
+        {
+            char[] symbols = new char[3] { ' ', 'X', 'O' };
+            int[] symbolSearch = new int[9];
+            int c = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (state.campo[i, j] == -1)
+                    {
+                        symbolSearch[c] = 0;
+                    }
+                    else if (state.campo[i, j] == 0)
+                    {
+                        symbolSearch[c] = 1;
+                    }
+                    else
+                    {
+                        symbolSearch[c] = 2;
+                    }
+                    c++;
+                }
+            }
+            Console.WriteLine(  $"+---+---+---+\n" +
+                                $"| {symbols[symbolSearch[0]]} | {symbols[symbolSearch[1]]} | {symbols[symbolSearch[2]]} |\n" +
+                                $"+---+---+---+\n" +
+                                $"| {symbols[symbolSearch[3]]} | {symbols[symbolSearch[4]]} | {symbols[symbolSearch[5]]} |\n" +
+                                $"+---+---+---+\n" +
+                                $"| {symbols[symbolSearch[6]]} | {symbols[symbolSearch[7]]} | {symbols[symbolSearch[8]]} |\n" +
+                                $"+---+---+---+\n");
+        }
     }
 }
